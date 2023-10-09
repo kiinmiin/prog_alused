936004c949b687b1aa10f87abc13aaef4f7ba42e
kiinmiin <rasmo.kiin@voco.ee>
2023-10-09 13:50:31 +0300

    different printing values

---
+++ print/examples/ex4.py
@@ -0,0 +1,7 @@
+name = "Rasmo"
+print(f"Hi {name}!") #Hi Rasmo!
+price_wo_tax = 20
+print(f"Price with tax is {price_wo_tax * 1.2}")
+# Price with tax is 24.0
+print("Sum: {1 + 2 + 3}") # Sum: {1 + 2 + 3}
+print(f"Sum: {1 + 2 + 3}") #Sum: 6
