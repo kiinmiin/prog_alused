812f6a8f6e6e006dddf198e6042a74c65e45d997
kiinmiin <rasmo.kiin@voco.ee>
2023-10-23 13:18:53 +0300

    mathematic exercises

--- mathematics/exercises/mathematics_exercises.py
+++ mathematics/exercises/mathematics_exercises.py
@@ -36,53 +36,57 @@ def find_average(num_a: int, num_b: int) -> float:
 
 def area_of_a_circle(radius: float) -> float:
     """Calculate and return the area of a circle."""
-    # Write your code here
+    circle_area = round(math.pi * math.pow(radius, 2), 2)
     return circle_area
 
 
 def area_of_an_equilateral_triangle(side_length: float) -> int:
     """Calculate and return the area of an equilateral triangle."""
-    # Write your code here
+    s = 3 * side_length / 2
+    triangle_area = round(math.sqrt(s * (s - side_length) ** 3 ))
     return triangle_area
 
 
 def calculate_discriminant(a: int, b: int, c: int) -> int:
     """Calculate discriminant with given variables and return the result."""
-    # Write your code here
+    discriminant = b ** 2 - 4 * a * c
     return discriminant
 
 
 def calculate_hypotenuse_length(a: int, b: int) -> float:
     """Return the length of hypotenuse when the lengths of the catheti are given."""
-    # Write your code here
+    c = math.sqrt(a ** 2 + b ** 2)
     return c
 
 
 def calculate_cathetus_length(a: int, c: int) -> float:
     """Return the length of cathetus when the lengths of the second cathetus and hypotenuse are given."""
-    # Write your code here
+    b = math.sqrt(c ** 2 - a ** 2)
     return b
 
 
 def time_converter(seconds: int) -> str:
     """Convert time in seconds to hours and minutes."""
-    # Write your code here
+    minutes = seconds // 60
+    hours = minutes // 60
+    minutes = minutes % 60
     return f"{seconds} sekundit on {hours} tund(i) ja {minutes} minut(it)."
 
 
 def student_helper(angle: int) -> str:
     """Return the sine and cosine of the given angle in degrees."""
-    # Write your code here
+    sine = round(math.sin(math.radians(angle)), 1)
+    cosine = round(math.cos(math.radians(angle)), 1)
     return f"Nurk: {angle}, siinus: {sine}, koosinus: {cosine}."
 
 
 def greetings(n: int) -> str:
     """Return a string that contains "Hey" n times."""
-    # Write your code here
+    lots_of_heys = "Hey" * n
     return lots_of_heys
 
 
 def adding_numbers(num_a: int, num_b: int) -> str:
     """Return given numbers added together as a string."""
-    # Write your code here
+    string_numbers = str(num_a) + str(num_b)
     return string_numbers
