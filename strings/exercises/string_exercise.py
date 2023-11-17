cb6db0bc9d70596c486fc9f9d31846a6d5977b9c
kiinmiin <rasmo.kiin@voco.ee>
2023-11-17 09:19:01 +0200

    string exercises

---
+++ strings/exercises/string_exercise.py
@@ -0,0 +1,21 @@
+"""String exercises."""
+
+
+"""Declaration of string-type variables"""
+first_name = "James"
+last_name = "Bond"
+
+"""Forming strings - names"""
+full_name = first_name + " " + last_name
+self_description_sentence = "My name is " + last_name + ", " + first_name + " " + last_name + "."
+
+"""Forming strings - cake string"""
+cake = "vahukoormarjadtäidispõhi"
+print("vahukoor\nmarjad\ntäidis\npõhi")
+
+"""String slicing"""
+original_string = "Programming is fun!"
+backwards = original_string[::-1]
+every_other = original_string[::2]
+first_word_reversed = "Programming"[::-1]
+
